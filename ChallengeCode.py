fffefdd7a6e7d2b14cbe054b7c06234ef27d0735
Myutalica <98805700+Myuta15@users.noreply.github.com>
2025-08-12 21:22:06 +0800

    Add files via upload

---
+++ ChallengeCode.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ")
+print("\t\t\t\t*\n\n\n"
+      "\t\t\t*\t\t*\n\n\n"
+      "\t\t*\t\t\t\t*\n\n\n"
+      "\t*\t\t\t\t\t\t*\n\n\n"
+      "*\t\t\tHi\t\033{}\033\t\t\t\t*\n\n\n"
+      "\t*\t\t\t\t\t\t*\n\n\n"
+      "\t\t*\t\t\t\t*\n\n\n"
+      "\t\t\t*\t\t*\n\n\n"
+      "\t\t\t\t*".format(name))
+
+
+
