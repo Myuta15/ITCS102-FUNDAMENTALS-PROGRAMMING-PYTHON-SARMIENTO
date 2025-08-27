fecadd80c2e306146fffb07904d64fa5f60089b8
Myutalica <98805700+Myuta15@users.noreply.github.com>
2025-08-27 10:57:34 +0800

    Add files via upload

---
+++ codechallenge3.py
@@ -0,0 +1,11 @@
+username = "MTS"
+password = "akg15"
+
+name = input("please input your username > ")
+password = input("Password?")
+
+if password.lower() == "akg15" and username == "MTS":
+		print("ACCESS GRANTED")
+
+else: 
+	print("ACCESS DENIED")
