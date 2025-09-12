b5c1fbb869628914b0f46f5c57610834148cddb1
Myutalica <98805700+Myuta15@users.noreply.github.com>
2025-09-12 11:25:31 +0800

    Add files via upload

---
+++ activity13.py
@@ -0,0 +1,8 @@
+#using for loop, ask use to input 10 numbers
+#after input, get the summation of all the number
+nim = 100
+for x in range(1,11):
+    number = eval(input("ENTER A NUMBER "))
+    print("WOW", nim)
+nim += number
+print(nim)
