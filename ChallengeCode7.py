a8e741d3a077c9b3c1ed3fc684fbe040b9a57d16
Myutalica <98805700+Myuta15@users.noreply.github.com>
2025-09-15 19:10:30 +0800

    Add files via upload

---
+++ ChallengeCode7.py
@@ -0,0 +1,12 @@
+#The summation of odd numbers / Code_Challenge_7.py 
+print("The Summation of ODD NUMBERS")
+num = int(input("Enter a number: "))
+
+result = 0
+
+for i in range(10):
+    if num % 2 == 1:
+        oddsum += num  
+
+print("The total of all the ODD numbers - - - > ", result)
+
