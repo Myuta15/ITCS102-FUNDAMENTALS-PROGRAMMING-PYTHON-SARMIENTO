8cde69691c6c7c48b12f38c60dfa12ff4cd46d96
Myutalica <98805700+Myuta15@users.noreply.github.com>
2025-09-17 10:33:01 +0800

    Add files via upload

---
+++ Code_Challenge8.py
@@ -0,0 +1,8 @@
+print("Multiplacation Table Maker")
+x = eval(input("Enter a number: "))
+
+print("Multiplaction table for:", x,)
+
+for z in range(1, 11):
+    y = x * z
+    print(x, "x", z, "=", y) 
---
+++ Code_Challenge9.py
@@ -0,0 +1,8 @@
+print("Countdown Timer Simulator")
+x = eval(input("Enter a number: "))
+print("Countdown Started:")
+for z in range(x, 0, -1,):
+    print(z)
+
+
+print("liftoff!")
